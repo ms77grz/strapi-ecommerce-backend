9155e5653f5ef430851b609e67cab49cd1f62aea
ms77grz <ms77grz@gmail.com>
2021-09-01 18:12:46 +0300

    Collections / Products

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db62d07bc59891995375b955a03d81ec'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b8b0aaa-e3de-4d2c-bcf1-e0ee70b371ea'
+};
